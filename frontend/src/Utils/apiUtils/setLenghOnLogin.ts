1516fe3feb424a550574dd631c591e3a94818aab
Senails <seffele@gmail.com>
2022-12-22 04:17:49 +0300

    ended frontend

--- frontend/src/Utils/apiUtils/setLenghOnLogin.ts
+++ frontend/src/Utils/apiUtils/setLenghOnLogin.ts
@@ -5,6 +5,7 @@ import { setUserProgress } from "./saveUserProgress";
 export async function setLenghtOnLogin() {
     let state = store.getState();
     let hrefBookNow = state.pleer.hrefparam;
+    if (!hrefBookNow) return;
     let bookMap = await getBookMap(hrefBookNow);
 
     if (bookMap!=='error' && bookMap.progress){
